7cbfb8393452acb7c1bce947cfaacea2d745cc73
Arsen Shuttov <shutt13a@gmail.com>
2019-01-20 18:43:48 +0200

    try to build

---
+++ build/precache-manifest.ef937bdb41b9acaa66c7b6419ef3794e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bffb9b9563fb40b73fbe8a0098c885c4",
+    "url": "/reddit-one/static/media/reddit-logo-full.bffb9b95.png"
+  },
+  {
+    "revision": "990cf5792a356642fe46e7dbe577ebab",
+    "url": "/reddit-one/static/media/reddit-icon.990cf579.png"
+  },
+  {
+    "revision": "1daade6c183d275cddb1",
+    "url": "/reddit-one/static/js/runtime~main.1daade6c.js"
+  },
+  {
+    "revision": "0da91fa544bfcee59491",
+    "url": "/reddit-one/static/js/main.0da91fa5.chunk.js"
+  },
+  {
+    "revision": "9d3020cce58c193e80a4",
+    "url": "/reddit-one/static/js/1.9d3020cc.chunk.js"
+  },
+  {
+    "revision": "0da91fa544bfcee59491",
+    "url": "/reddit-one/static/css/main.7a5cce76.chunk.css"
+  },
+  {
+    "revision": "9d3020cce58c193e80a4",
+    "url": "/reddit-one/static/css/1.577657d3.chunk.css"
+  },
+  {
+    "revision": "5db049283ff328a2ec5bd92bfb356e35",
+    "url": "/reddit-one/index.html"
+  }
+];
