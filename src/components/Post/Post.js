b54365b414db5f8d3416a05c53d884cfbe585882
13shutt <shutt13a@gmail.com>
2019-02-02 08:43:44 +0200

    del video

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import numeral from 'numeral'
 
-import { Ups, PostHeader, PostTitle, PostFooterIcon, Wrapper, Video } from './styles'
+import { Ups, PostHeader, PostTitle, PostFooterIcon, Wrapper } from './styles'
 
-const Post = ({data: {ups, subreddit_name_prefixed, author, secure_media, is_video, title, num_comments}}) => (
+const Post = ({data: {ups, subreddit_name_prefixed, author, secure_media, thumbnail, is_video, title, num_comments}}) => (
   <Wrapper container>
     <Wrapper posts>
       <Ups>{numeral(ups).format('0a')}</Ups>
       <Wrapper content>
         <PostHeader subreddit={subreddit_name_prefixed} author={author} />
         <PostTitle title={title} />
-
-        {is_video ? (<Video secure_media={secure_media} />) : null}
-
         <Wrapper footer>
           <PostFooterIcon faClass={"fa-comments"} children={numeral(num_comments).format('0a')} text={"Comments"}/>
           <PostFooterIcon faClass={"fa-share"} text={"Share"}/>
--- src/components/Post/styles/index.js
+++ src/components/Post/styles/index.js
@@ -3,6 +3,5 @@ import { PostHeader } from './PostHeader'
 import { PostTitle } from './PostTitle'
 import { PostFooterIcon } from './PostFooterIcon'
 import { Wrapper } from './Wrapper'
-import { Video } from './Video'
 
-export { Ups, PostHeader, PostTitle, PostFooterIcon, Wrapper, Video }
+export { Ups, PostHeader, PostTitle, PostFooterIcon, Wrapper }
