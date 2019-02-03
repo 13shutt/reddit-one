7f8bbc88238bf2f1d96da057cb51d0268068d285
13shutt <shutt13a@gmail.com>
2019-02-03 13:03:06 +0200

    require -> import

--- src/components/Header/styles/Logo.js
+++ src/components/Header/styles/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import redditLogo from '../../../assets/images/reddit-logo-full.png'
 
 const StyledDiv = styled.div`
   width: 140px;
@@ -11,7 +12,7 @@ const StyledDiv = styled.div`
 export const Logo = ({ link, click }) => (
   <StyledDiv>
     <a href={link} onClick={click}>
-      <img src={require('../../../assets/images/reddit-logo-full.png')} alt="reddit-logo"/>
+      <img src={redditLogo} alt="reddit-logo"/>
     </a>
   </StyledDiv>
 )
