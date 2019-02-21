1412b6baeb0ad2e74f1c8daca0881375abe05766
13shutt <shutt13a@gmail.com>
2019-02-21 21:52:13 +0200

    refactor

--- src/view/PostPage/PostPage.js
+++ src/view/PostPage/PostPage.js
@@ -1,23 +1,9 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
+
 import PostSingle from '../../components/Post/PostSingle'
 import Comments from '../../components/Comments'
 import PostHeader from '../../components/PostHeader'
-
-const StyledDiv = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin: 0 15vw 0 15vw;
-  padding: 70px 15px;
-  border-left: 1px solid rgba(0,0,0,0.4);
-  border-right: 1px solid rgba(0,0,0,0.4);
-  background: #eeeeee;
-`
-
-const StyledBody = styled.body`
-  background: #2e2f30;
-`
+import { StyledBody, StyledDiv } from '../../components/PostPageWrappers'
 
 class PostPage extends Component {
 
--- src/view/Posts/Posts.js
+++ src/view/Posts/Posts.js
@@ -1,54 +1,30 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
 import Loader from 'react-loader-spinner'
 
 import Post from '../../components/Post/Post'
-
-const StyledSection = styled.section`
-  margin-top: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`
-
-const StyledDiv = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 89vh;
-  margin-top: 40px;
-  background: #dae0e6;
-`
+import { StyledDiv, StyledSection } from '../../components/PostsWrappers'
 
 class Posts extends Component {
   constructor(props) {
     super(props)
 
     this.props.history.listen(async () => {
-      await console.log('History changed')
+      await console.log('history changed')
+      // тут трабл, без верхнього рядку втикається на один пункт в історії
       await this.fetchReddit(this.props.match.url)
     })
   }
 
   fetchReddit(url) {
-    console.log('fetch posts from posts')
-    switch (url) {
-      case '/':
-      this.props.actions.fetchPosts('popular', 10)
-        break;
-      case '/r/popular':
-      this.props.actions.fetchPosts('popular', 10)
-        break;
-      case '/r/all':
-      this.props.actions.fetchPosts('all', 10)
-        break;
-      case '/r/original':
-      this.props.actions.fetchOriginalPosts(10)
-        break;
-      default:
-      this.props.actions.fetchPosts('popular', 10)
+    console.log('fetch posts from posts', this.props.match.url)
+    const fetchPosts = {
+      '/': () => this.props.actions.fetchPosts('popular', 10),
+      '/r/popular': () => this.props.actions.fetchPosts('popular', 10),
+      '/r/all': () => this.props.actions.fetchPosts('all', 10),
+      '/r/original': () => this.props.actions.fetchOriginalPosts(10)
     }
+
+    return fetchPosts[url] ? fetchPosts[url]() : this.props.actions.fetchPosts('popular', 10)
   }
 
   async componentDidMount () {
