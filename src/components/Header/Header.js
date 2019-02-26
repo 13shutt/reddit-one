70648cb9536a5e69025a537d837437aaf77294dd
gt60 <gt60@ya.ru>
2019-02-26 18:29:34 +0200

    oAuth

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import { Input, Icon, User, Logo, ContentIcon, Wrapper } from './styles'
 
 
 const Header = (props) => {
-  
+
   const [classname, setClassname] = useState('fa-line-chart')
   const [postType, setPostType] = useState('Popular')
 
@@ -16,7 +16,7 @@ const Header = (props) => {
   }
 
   const myURL = {
-    dev: 'http://localhost:3000/',
+    dev: 'http://r.fenko.ga',
     prod: 'https://13shutt.github.io/reddit-one/'
   }
 
@@ -31,34 +31,51 @@ const Header = (props) => {
     await fetch('https://www.reddit.com/api/v1/access_token', {
       method: 'POST',
       headers: {
-        'Authorization': `grant_type=authorization_code&code=${code}&redirect_uri=${myURL.dev}`
-      }
+        // 'Authorization': "Basic 8qWPr5D31K_-Kg:OFMmXC4z45jDZqs_Wb1qbwZcGP4",
+        "Authorization": "Basic OHFXUHI1RDMxS18tS2c6T0ZNbVhDNHo0NWpEWnFzX1diMXFid1pjR1A0",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: `grant_type=authorization_code&code=${code}&redirect_uri=http://r.fenko.ga`
+
     })
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        console.log("TOKEN: " + data.access_token)
+
+        fetch('https://oauth.reddit.com/api/v1/me', {
+          method: 'GET',
+          headers: {
+            "Authorization": `Bearer ${data.access_token}`,
+          },
+        })
+          .then(res => res.json())
+          .then(data => console.log(data))
+      })
   }
-  
+
+
+
   return (
     <header>
       <Wrapper header>
 
         <Link to="/">
-          <Logo link={myURL.dev} click={() => iconFunction('Popular', 'fa-line-chart')}/>
+          <Logo link={myURL.dev} click={() => iconFunction('Popular', 'fa-line-chart')} />
         </Link>
 
-        <ContentIcon classname={classname} postType={postType}/>
+        <ContentIcon classname={classname} postType={postType} />
 
-        <Input placeholder="Search Reddit"/>
+        <Input placeholder="Search Reddit" />
 
         <Wrapper icons>
-          <Icon classname="fa-line-chart" type="popular"  click={() => iconFunction('Popular', 'fa-line-chart')}/>
-          <Icon classname="fa-bar-chart" type="all"  click={() => iconFunction('All', 'fa-bar-chart')}/>
-          <Icon classname="fa-pie-chart" type="original"  click={() => iconFunction('Original', 'fa-pie-chart')}/>
+          <Icon classname="fa-line-chart" type="popular" click={() => iconFunction('Popular', 'fa-line-chart')} />
+          <Icon classname="fa-bar-chart" type="all" click={() => iconFunction('All', 'fa-bar-chart')} />
+          <Icon classname="fa-pie-chart" type="original" click={() => iconFunction('Original', 'fa-pie-chart')} />
         </Wrapper>
 
         <Wrapper btns>
           <Button primary>
-            <a href={`https://www.reddit.com/api/v1/authorize?client_id=uDmsZCsIwBBrjg&response_type=code&state=lupazapypyapypaza&redirect_uri=${myURL.dev}&duration=temporary&scope=identity`}>LOG IN</a>
+            <a href={`https://www.reddit.com/api/v1/authorize?client_id=8qWPr5D31K_-Kg&response_type=code&state=lupazapypyapypaza&redirect_uri=${myURL.dev}&duration=temporary&scope=identity`}>LOG IN</a>
           </Button>
           <Button onClick={getToken}>SING UP</Button>
         </Wrapper>
@@ -69,5 +86,5 @@ const Header = (props) => {
     </header>
   );
 }
- 
+
 export default Header; 
